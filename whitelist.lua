3507586d32b47e616995573c65758dcf450235c8
CodeDev9 <127266511+codedev9@users.noreply.github.com>
2023-08-06 17:59:59 +1200

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,3 +1,8 @@
-local whitelist = {
-  "4808214912", "4869389269"
+local whitelist = {"
+"7grandad66", "BedwarsFixYourGame9"
 }
+local function whitelist.lplrtype(plr)
+   if plr.Name == whitelist then
+      return plr
+   end
+end
